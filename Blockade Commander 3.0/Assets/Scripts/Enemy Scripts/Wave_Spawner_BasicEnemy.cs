24a8b22ad91c49493aa5f35b87b246ff87afbe95
mncarey <madisonniccarey@gmail.com>
2026-02-24 16:15:15 -0800

    spawner work

--- Blockade Commander 3.0/Assets/Scripts/Enemy Scripts/Wave_Spawner_BasicEnemy.cs
+++ Blockade Commander 3.0/Assets/Scripts/Enemy Scripts/Wave_Spawner_BasicEnemy.cs
@@ -14,6 +14,8 @@ public class Wave_Spawner_BasicEnemy : MonoBehaviour
     public Vector2 spawnAreaMax;
     private Vector3 randomSpawn;
 
+    public int enemiesAlive = 0;
+
     private bool canSpawn = false;
 
     private void Start()
@@ -35,7 +37,6 @@ public class Wave_Spawner_BasicEnemy : MonoBehaviour
             SpawnEnemy();
         }
         */
-        
     }
 
     public void SpawnEnemy()
@@ -43,6 +44,18 @@ public class Wave_Spawner_BasicEnemy : MonoBehaviour
         for(int i = 0; i< spawnPoints.Length; i++)
         {
             Instantiate(BasicEnemy, spawnPoints[i].position, Quaternion.identity);
+            enemiesAlive++;
         }     
     }
+
+    public void EnemyDied()
+    {
+        enemiesAlive--;
+
+        if (enemiesAlive <= 0)
+        {
+            Debug.Log("Wave Complete!");
+        }
+    }
+
 }
