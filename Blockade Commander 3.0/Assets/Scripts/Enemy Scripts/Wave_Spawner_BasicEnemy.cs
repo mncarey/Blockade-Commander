810623f087aab0fa19e5ddd7cb2f31b4ca555e9b
DeclanKirby <declanpkirby@gmail.com>
2026-02-25 16:08:10 -0800

    Created Enemy Randomizer again

--- Blockade Commander 3.0/Assets/Scripts/Enemy Scripts/Wave_Spawner_BasicEnemy.cs
+++ Blockade Commander 3.0/Assets/Scripts/Enemy Scripts/Wave_Spawner_BasicEnemy.cs
@@ -10,6 +10,12 @@ public class Wave_Spawner_BasicEnemy : MonoBehaviour
     public StartWaveButton startWaveRef;
     public GameObject enemyDefeatPopup;
 
+
+    public GameObject sloopRef;
+    public GameObject brigRef;
+    public GameObject gallRef;
+    private GameObject[] enemyPrefabs;
+
     //random points
     public Vector2 spawnAreaMin;
     public Vector2 spawnAreaMax;
@@ -21,6 +27,10 @@ public class Wave_Spawner_BasicEnemy : MonoBehaviour
 
     private void Start()
     {
+        enemyPrefabs = new GameObject[3];
+        enemyPrefabs[0] = sloopRef;
+        enemyPrefabs[1] = brigRef;
+        enemyPrefabs[2] = gallRef;
         /*
         Debug.Log("spawning wave");
         SpawnEnemy();
@@ -50,7 +60,9 @@ public class Wave_Spawner_BasicEnemy : MonoBehaviour
     {
         for(int i = 0; i< spawnPoints.Length; i++)
         {
-            GameObject enemy = Instantiate(BasicEnemy, spawnPoints[i].position, Quaternion.identity);
+            int randomIndex = Random.Range(0, enemyPrefabs.Length);
+            GameObject randomEnemy = enemyPrefabs[randomIndex];
+            GameObject enemy = Instantiate(randomEnemy, spawnPoints[i].position, Quaternion.identity);
             enemiesAlive++;
 
             enemy.GetComponent<BasicEnemy>().waveSpawnerRef = this;
