d8b14d65bc10d829aef180df07e90cf3610f04b4
DeclanKirby <declanpkirby@gmail.com>
2026-04-05 13:52:05 -0700

    Created Locked slot prefab for viewing in bar

--- Blockade Commander 3.0/Assets/Scripts/FortMenu/fortSlot.cs
+++ Blockade Commander 3.0/Assets/Scripts/FortMenu/fortSlot.cs
@@ -28,7 +28,8 @@ public class fortSlot : MonoBehaviour
     private string lighthouseText = "A primarily defensive fortification that forces enemies to attack it if they are within range. Cannot damage ranged enemies.";
     private string cannonText = "A medium range and damage fortification that assaults enemy ships with a quick rate of fire";
     private string mortarText = "A long range heavy damage fortification, which cannot attack enemies that get too close, low rate of fire";
-    //private string wallText = "A wall, simple as";
+    private string wallText = "A wall, simple as";
+    private string lockedText = "Ascend to unlock.";
 
     private string fortName;
 
@@ -74,9 +75,13 @@ public class fortSlot : MonoBehaviour
         }
         if(fortName == "Wall")
         {
-            //content = wallText;
+            content = wallText;
             currentFort = wall;
             
+        }
+        if(fortName == "LockedSlot")
+        {
+            content = lockedText;
         }
         
     }
