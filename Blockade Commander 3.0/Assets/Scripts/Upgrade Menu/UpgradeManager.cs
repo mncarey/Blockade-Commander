e2331a248421e6efbf659280b5f503005be8e494
mncarey <madisonniccarey@gmail.com>
2026-04-20 19:21:19 -0700

    Update UpgradeManager.cs

--- Blockade Commander 3.0/Assets/Scripts/Upgrade Menu/UpgradeManager.cs
+++ Blockade Commander 3.0/Assets/Scripts/Upgrade Menu/UpgradeManager.cs
@@ -15,6 +15,8 @@ public class UpgradeManager : MonoBehaviour
     public int wallPrice = 30;//base price
     public int cannonPrice = 30;//base price
 
+    public AscensionManager ascensionRef;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
