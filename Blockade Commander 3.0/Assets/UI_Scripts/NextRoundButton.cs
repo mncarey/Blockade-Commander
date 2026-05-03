13a173c1c5afd4665d872d7d1c5b0623d6aaaba5
mncarey <madisonniccarey@gmail.com>
2026-05-03 11:43:55 -0700

    added to combined scene

--- Blockade Commander 3.0/Assets/UI_Scripts/NextRoundButton.cs
+++ Blockade Commander 3.0/Assets/UI_Scripts/NextRoundButton.cs
@@ -20,8 +20,9 @@ public class NextWaveButton : MonoBehaviour
     public GameObject fortMenuRef;
     public GameObject exitFortMenuRef;
     public GameObject fortRemoveRef;
-    public PlayerFortress playerFortRef;
+    public GameObject fortNumberHolderRef;
 
+    public PlayerFortress playerFortRef;
     public FortUIManager fortUIManagerRef;
     public ResourceUI resourceRef;
     public UpgradeButton upgradeButtonRef;
@@ -86,5 +87,8 @@ public class NextWaveButton : MonoBehaviour
 
         //Reset the fortification type limit to 0
         placingScriptRef.ResetFortTypePlacement();
+
+        //activate FortNumberHolder
+        fortNumberHolderRef.SetActive(true);
     }
 }
