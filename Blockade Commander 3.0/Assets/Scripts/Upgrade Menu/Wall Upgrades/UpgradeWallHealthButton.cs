e7fbd0dc87fe21edac571549babc49a4076721a3
mncarey <madisonniccarey@gmail.com>
2026-05-06 20:14:49 -0700

    coin sound effects

--- Blockade Commander 3.0/Assets/Scripts/Upgrade Menu/Wall Upgrades/UpgradeWallHealthButton.cs
+++ Blockade Commander 3.0/Assets/Scripts/Upgrade Menu/Wall Upgrades/UpgradeWallHealthButton.cs
@@ -6,6 +6,7 @@ public class UpgradeWallHealthButton : MonoBehaviour
 
     [SerializeField] private UpgradeManager upgradeManagerRef;
     public UpgradesText upgradesTextRef;
+    public AudioSource audioRef;
     public GameObject noMoneyPopup;
     public GameObject noLvlsPopup;
 
@@ -41,6 +42,9 @@ public class UpgradeWallHealthButton : MonoBehaviour
             noMoneyPopup.gameObject.SetActive(true);
             return;
         }
+
+        //play coin sound
+        audioRef.Play();
 
         //subtract gold
         resourceUIRef.gold -= upgradeManagerRef.wallPrice;
